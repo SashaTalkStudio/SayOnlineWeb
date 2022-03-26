c4a6d94efacadc6a60ff8a042a145f0dbf7db9df
SashaTalk <takensisi666@gmail.com>
2022-03-26 22:03:02 +0300

    More mobile adaptivity and CSS animations

---
+++ script.js
@@ -0,0 +1,6 @@
+chat_click = function() {
+    document.getElementById("main_column").classList.remove("mob_hidden");
+}
+chat_close = function() {
+    document.getElementById("main_column").classList.add("mob_hidden");
+}
