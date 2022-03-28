11a5021a7b99dc41e6eff2a34e4e35e0e608c153
SashaTalk <takensisi666@gmail.com>
2022-03-28 13:26:04 +0300

    Nice slideout for mobiles

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 chat_click = function() {
+    document.getElementById("main_column").classList.remove("slideout");
     document.getElementById("main_column").classList.remove("mob_hidden");
 }
 chat_close = function() {
-    document.getElementById("main_column").classList.add("mob_hidden");
+    document.getElementById("main_column").classList.add("slideout");
 }
