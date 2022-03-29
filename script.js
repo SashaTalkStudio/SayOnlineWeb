e69b1f37ac36e0e542aa78906ff8601e1f178d1d
SashaTalk <takensisi666@gmail.com>
2022-03-29 12:46:49 +0300

    Increased code readability

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
-var count = 2 // just for demonstration
-var currdate = "15.03.2022" // just for demonstration
+// just for demonstration
+count = 2
+currdate = "15.03.2022"
+
+onpageload = function() {
+    // put elements to variables
+    messages = document.getElementById("messages")
+    input = document.getElementById("input")
+    sendbutton = document.getElementById("send")
+
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            sendbutton.click();
+        }
+    });
+}
+
 chat_click = function() {
     document.getElementById("main_column").classList.remove("slideout");
     document.getElementById("main_column").classList.remove("mob_hidden");
 }
+
 chat_close = function() {
     document.getElementById("main_column").classList.add("slideout");
 }
+
 send = function() {
     if (currdate != getcurrdate()) {
-        document.getElementById("messages").innerHTML = document.getElementById("messages").innerHTML + '<div class="date_container" id="' + getcurrdate() + '"><div class="date">' + getcurrdate() + '</div></div>'
+        messages.innerHTML += '<div class="date_container" id="' + getcurrdate() + '"><div class="date">' + getcurrdate() + '</div></div>'
     }
     currdate = getcurrdate()
-    if ( document.getElementById("input").value.length != 0 ) {
+    if ( input.value.length != 0 ) {
         count = count + 1
-        document.getElementById("messages").innerHTML = document.getElementById("messages").innerHTML + '<div class="mymessagecell" id="msg' + count + '"><div class="message">' + document.getElementById("input").value + '</div><div class="time">' + lss0(new Date().getHours()) + ":" + lss0(new Date().getMinutes()) + '</div><div class="msg_tools"><button class="msg_tools_delete" onclick="delmsg(\'' + count + '\')">Delete</button></div></div>'
-        document.getElementById("input").value = ""
-        document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
+        messages.innerHTML += '<div class="mymessagecell" id="msg' + count + '"><div class="message">' + input.value + '</div><div class="time">' + getcurrtime() + '</div>' + initmsgtools(count) + '</div>'
+        input.value = ""
+        messages.scrollTop = messages.scrollHeight
     }
 }
-onpageload = function() {
-    var input = document.getElementById("input")
-    input.addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-            document.getElementById("send").click();
-        }
-    });
-}
+
 delmsg = function(_n) {
     document.getElementById("msg" + _n).remove()
 }
+
 lss0 = function(_time) {
     if (_time < 10) {
         _time = "0" + _time
     }
     return (_time)
 }
+
 getcurrdate = function() {
     return lss0((new Date).getDate()) + "." + lss0((new Date).getMonth() + 1) + "." + ((new Date).getYear() + 1900)
+}
+
+getcurrtime = function() {
+    return lss0(new Date().getHours()) + ":" + lss0(new Date().getMinutes())
+}
+
+initmsgtools = function(_msg) {
+    return '<div class="msg_tools"><button class="msg_tools_delete" onclick="delmsg(\'' + _msg + '\')">Delete</button></div>'
 }
