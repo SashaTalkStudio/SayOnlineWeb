48e1efe62e276337a0272bded4437fc2fec10dbe
Anatoly Chizhikov <78175165+Tolinme@users.noreply.github.com>
2022-06-09 19:25:50 +0300

    Bug fixes and changing old stuff (#7)
    * Исправление бага и изменение старого
    
    * Update index.html
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * Update index.html
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * Update script.js
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * Update script.js
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * Update script.js
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * Update index.html
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * Update index.html
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * Update script.js
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>
    
    * I will no longer work with Sasha
    
    * i'm about to drown
    
    Co-authored-by: SashaTalk <76222964+SashaTalk@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
 // just for demonstration
 count = 2
 currdate = "15.03.2022"
+messages = document.getElementById("messages")
+sendbutton = document.getElementById("send")
+input = document.getElementById("input")
 
-onpageload = function() {
-    // put elements to variables
-    messages = document.getElementById("messages")
-    input = document.getElementById("input")
-    sendbutton = document.getElementById("send")
+input.addEventListener("keyup", event => {
+    if (event.keyCode === 13) {
+        send()
+    }
+});
 
-    input.addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-            sendbutton.click();
-        }
-    });
+function chat_click() {
+    document.getElementById("main_column").classList.remove("slideout", "mob_hidden")
 }
 
-chat_click = function() {
-    document.getElementById("main_column").classList.remove("slideout")
-    document.getElementById("main_column").classList.remove("mob_hidden")
-}
-
-chat_close = function() {
+function chat_close() {
     document.getElementById("main_column").classList.add("slideout")
 }
 
-send = function() {
-    if (currdate != getcurrdate()) {
-        messages.innerHTML += '<div class="date_container" id="' + getcurrdate() + '"><div class="date">' + getcurrdate() + '</div></div>'
-    }
-    currdate = getcurrdate()
-    if ( input.value.length != 0 ) {
+function send() { 
+    if ( input.value.trim().length != 0 ) {
+        if (currdate != getcurrdate()) {
+            messages.innerHTML += '<div class="date_container" id="' + getcurrdate() + '"><div class="date">' + getcurrdate() + '</div></div>'
+        }
+        currdate = getcurrdate()
+
         count = count + 1
-        messages.innerHTML += '<div class="mymessagecell" id="msg' + count + '"><div class="message">' + input.value + '</div><div class="time">' + getcurrtime() + '</div>' + initmsgtools(count) + '</div>'
+        // prevention of html code injection
+        let re = /</gi;
+        let mess = input.value.replace(re, "&lt;")
+        messages.innerHTML += `<div class="mymessagecell" id="msg${count}"><div class="message">${mess}<time>${getcurrtime()}</time></div>${initmsgtools(count)}</div>`
         input.value = ""
         messages.scrollTop = messages.scrollHeight
     }
 }
 
-delmsg = function(_n) {
+function delmsg(_n) {
     document.getElementById("msg" + _n).remove()
 }
 
-lss0 = function(_time) {
+function lss0(_time) {
     if (_time < 10) {
         _time = "0" + _time
     }
     return (_time)
 }
 
-getcurrdate = function() {
-    return lss0((new Date).getDate()) + "." + lss0((new Date).getMonth() + 1) + "." + ((new Date).getYear() + 1900)
+function getcurrdate() {
+    return lss0((new Date).getDate()) + "." + lss0((new Date).getMonth() + 1) + "." + ((new Date).getFullYear())
 }
 
-getcurrtime = function() {
+function getcurrtime() {
     return lss0(new Date().getHours()) + ":" + lss0(new Date().getMinutes())
 }
 
-initmsgtools = function(_msg) {
+function initmsgtools(_msg) {
     return '<div class="msg_tools"><button class="msg_tools_delete" onclick="delmsg(\'' + _msg + '\')">Delete</button></div>'
 }
